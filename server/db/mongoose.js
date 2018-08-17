18608faedbbec5297c3197c06fd111f533d087b8
Blayne Storm <vlad.romila@info.uaic.ro>
2018-08-17 23:11:01 +0300

    Setup app for heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,7 @@
 var mongoose = require("mongoose");
 
 mongoose.Promise = global.Promise; //we set up mongoose to use promises instead of callbacks
-mongoose.connect("mongodb://localhost:27017/TodoApp");
+mongoose.connect("mongodb://<BlayneStorm>:<Password98>@ds225442.mlab.com:25442/node-todo-api-db" || "mongodb://localhost:27017/TodoApp");
 
 module.exports = {
     mongoose: mongoose
--- server/server.js
+++ server/server.js
@@ -7,6 +7,7 @@ var {Todo} = require("./models/todo.js");
 var {User} = require("./models/user.js");
 
 var app = express();
+const port = process.env.PORT || 3000; //this is set if the app is running on heroku
 
 app.use(bodyParser.json()); //3rd party middlewear - access smth off of the library (not custom - which will be a function arg (req, res, next) -> {...}) - with this in place we can now send JSON to our express application
 
@@ -55,8 +56,8 @@ app.get("/todos/:id", (req, res) => {
     });
 });
 
-app.listen(3000, () => {
-    console.log("Started on port 3000");
+app.listen(port, () => {
+    console.log(`Started up at port ${port}`);
 });
 
 module.exports = {
