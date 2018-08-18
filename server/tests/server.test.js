4ea92530217dc721982bc1d239d0fc0e4301b9b5
Blayne Storm <vlad.romila@info.uaic.ro>
2018-08-18 22:24:34 +0300

    Add tests for PATCH /todos/:id

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -10,7 +10,9 @@ const todos = [{
     text: "First test todo"
 }, {
     _id: new ObjectID(),
-    text: "Second test todo"
+    text: "Second test todo",
+    completed: true,
+    completedAt: 333
 }];
 
 //insertMany (mongoose method) - takes an array (like the one above) and inserts all of the documents into the collection
@@ -160,6 +162,44 @@ describe("DELETE /todos/:id", () => {
             .expect(404)
             .expect((res) => {
                 expect(res.body.textResponse).toBe("Id invalid");
+            })
+            .end(done);
+    });
+});
+
+describe("PATCH /todos/:id", () => {
+    it("should update the todo", (done) => {
+        var hexId = todos[0]._id.toHexString();
+        
+        request(app)
+            .patch(`/todos/${hexId}`)
+            .send({
+                text: "Text changed (test 1)", 
+                completed: true
+            })
+            .expect(200)
+            .expect((res) => {
+                expect(res.body.todo.text).toBe("Text changed (test 1)");
+                expect(res.body.todo.completed).toBe(true);
+                expect(res.body.todo.completedAt).toBeA("number");
+            })
+            .end(done);
+    });
+    
+    it("should clear completedAt when todo is not completed", (done) => {
+        var hexId = todos[1]._id.toHexString();
+        
+        request(app)
+            .patch(`/todos/${hexId}`)
+            .send({
+                text: "Text changed (test 2)", 
+                completed: false
+            })
+            .expect(200)
+            .expect((res) => {
+                expect(res.body.todo.text).toBe("Text changed (test 2)");
+                expect(res.body.todo.completed).toBe(false);
+                expect(res.body.todo.completedAt).toNotExist();
             })
             .end(done);
     });
